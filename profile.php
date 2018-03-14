19a8e537bb52063f7460446c61bff55e1a7c9a25
jsac90 <jsac90@gmail.com>
2018-03-14 01:57:03 -0700

    added days until due and some color coding just for funsies

--- profile.php
+++ profile.php
@@ -20,6 +20,19 @@ $error = "";
 $date = date('Y-m-d H:i:s');
 $taskname = $task_total['taskname'];
 $taskcount = mysqli_num_rows($tq);
+$pastduecount = 
+mysqli_num_rows(mysqli_query($db, 
+"select a.*
+from tasks a 
+where a.userid = $login_session and
+timestampdiff(DAY,sysdate(),DUEDATE)+1 <= 0 
+order by isnull(duedate) asc, duedate asc"));
+
+if ($pastduecount == 1){
+	$pastduesentence = "1 TASK IS PAST DUE!";
+} elseif ($pastduecount > 1) {
+	$pastduesentence = "$pastduecount TASKS ARE PAST DUE!";
+}
 
 if (!isset($_SESSION['login_user']) || $_SESSION['login_user'] == ''){
 	header("location: tasks.php");
@@ -45,7 +58,8 @@ mysqli_close($db); // Closing Connection
 <br /> <br />
 <h2>Welcome Back, <?php echo "$email"; ?></h2>
 <br /> <br />
-You currently have <b><?php echo "$taskcount"; ?></b> open tasks! <br><br>
+You currently have <b><?php echo "$taskcount"; ?></b> open tasks! <BR>
+<font color="red"><b><?php if ($pastduecount > 0){echo "$pastduesentence";} ?></b></font><br><br>
 
 <form action="add.php" method="post">
 <input name="add" type="submit" value="ADD A TASK">
@@ -76,13 +90,23 @@ foreach ($rows as $row){
 	} else {
 		$taskduedate = $row['duedate'];
 	}
+	$daysuntildue = $row['diff'];
+	
+	if ($daysuntildue <=0 && !empty($row['duedate'])){
+		$taskname = '<FONT COLOR=RED>'.$taskname.'</FONT>';
+	} elseif ($daysuntildue <= 2 && $daysuntildue > 0){
+		$taskname = '<FONT COLOR=ORANGE>'.$taskname.'</FONT>';
+	} elseif (empty($row['duedate'])){
+		$taskname = '<FONT COLOR=#06a837>'.$taskname.'</FONT>';
+	}
+	
 	echo 
 	"
 	<b>$taskname</b> <br> 
 	Due Date: $taskduedate <br>
 	Created on $created.<Br><br>
-	Description: <br>$taskdesc<br><br>
-	
+	Days until due: $daysuntildue <br><br>
+	<u>Description:</u> <br>$taskdesc<br><br>
 	"; 
 	
 ?>
