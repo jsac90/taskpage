7df373d9fb8d82dc551b7db54e43068af3517351
jsac90 <jsac90@gmail.com>
2018-03-20 20:40:31 -0700

    fixed cookie deletion and redirect

--- logout.php
+++ logout.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
+setcookie('jstaskpagelogin','', time() - 3600,'/');
+unset($_COOKIE['jstaskpagelogin']);
 if(session_destroy()) // Destroying All Sessions
 {
 // set the expiration date to one hour ago
-setcookie("jstaskpagelogin", "", time() - 3600);
 header("Location: tasks.php"); // Redirecting To Home Page
 }
 ?>   
